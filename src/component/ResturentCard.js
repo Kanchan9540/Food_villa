0c21613d7ca45a512797ae20a6e7fa59d0b215d7
KANCHAN <132828347+Kanchan9540@users.noreply.github.com>
2024-02-04 17:06:18 +0530

    updated

--- src/component/ResturentCard.js
+++ src/component/ResturentCard.js
@@ -20,7 +20,7 @@ const ResturentCard = ({name,
     const {user} = useContext(userContext);
 
     return (
-        <div className="w-52 p-5 m-2 shadow-xl bg-black text-white h-96 rounded-lg transition-all duration-300 ease-out hover:transform hover:scale-105">
+        <div className="w-52 p-2 m-2 shadow-xl bg-black text-white h-96 rounded-lg transition-all duration-300 ease-out hover:transform hover:scale-105">
             <img src={img_cdn_url + cloudinaryImageId}/>
              <h2 className="font-bold text-2xl">{name}</h2>
             <h3 className="text-wrap truncate">{itemToSlice.join(",")}</h3>
